057e2f8ae6735726a9bb496ba45ba2d799a44d8c
abhizarkar7551 <abhizarkar@yahoo.com>
2026-01-07 22:57:57 +0530

    Create main.tf

---
+++ modules/gke/main.tf
@@ -0,0 +1,25 @@
+resource "google_container_cluster" "gke" {
+  name     = var.cluster_name
+  location = var.region
+
+  network    = var.network
+  subnetwork = var.subnetwork
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  cluster    = google_container_cluster.gke.name
+  location   = var.region
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
